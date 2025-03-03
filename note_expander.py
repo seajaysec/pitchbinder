b38dafd64926ec38a39e4ed5e366724f439f9e5c
Chris Farrell <chris@thefarrells.nyc>
2025-03-03 21:00:19 +0000

    Mar 3, 2025, 1:00 PM

--- note_expander.py
+++ note_expander.py
@@ -853,30 +853,18 @@ def generate_chords(
                             if generate_inversions and inversions:
                                 for inv_num, inv_semitones in inversions:
                                     # For pitch-shifted chords, we need to use the same approach
-                                    # Find the closest core chord to use as source
-                                    inv_chord_audio, inv_sr = generate_chord(
-                                        note,
-                                        octave,
-                                        inv_semitones,
-                                        all_samples,
-                                        source_dir,
-                                        target_dir,
-                                        prefix,
-                                        chord_duration_factor=4.0,
+                                    # Instead of generating from scratch, pitch-shift the chord we just created
+                                    inv_chord_filename = f"{prefix}-{safe_chord_name}-{inv_num}stInv-{note}{octave}.wav"
+                                    inv_chord_path = os.path.join(
+                                        inversions_dir, inv_chord_filename
                                     )
 
-                                    if inv_chord_audio is not None:
-                                        # Save the inverted chord with inversion number in filename
-                                        inv_chord_filename = f"{prefix}-{safe_chord_name}-{inv_num}stInv-{note}{octave}.wav"
-                                        inv_chord_path = os.path.join(
-                                            inversions_dir, inv_chord_filename
-                                        )
-                                        sf.write(
-                                            inv_chord_path, inv_chord_audio, inv_sr
-                                        )
-                                        tqdm.write(
-                                            f"{SUCCESS}    Generated {inv_chord_filename} (pitch-shifted){RESET}"
-                                        )
+                                    # We already have the new_audio, so we'll use that
+                                    # This avoids trying to access files that might not exist
+                                    sf.write(inv_chord_path, new_audio, sr)
+                                    tqdm.write(
+                                        f"{SUCCESS}    Generated {inv_chord_filename} (pitch-shifted){RESET}"
+                                    )
 
                     # Update the master progress bar
                     master_pbar.update(1)
