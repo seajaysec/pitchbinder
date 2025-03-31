5b90edc678e0bf2610aa31b3ce80f73db427bfa4
Chris Farrell <chris@thefarrells.nyc>
2025-03-31 04:45:58 +0000

    Mar 30, 2025, 9:45 PM

--- note_expander.py
+++ note_expander.py
@@ -2880,7 +2880,9 @@ def interactive_mode():
                     keep_artifacts=options_dict["keep_artifacts"],
                     chord_qualities=chord_qualities,
                     generate_inversions=generate_inversions,
-                    overwrite=options_dict["overwrite"],  # Add overwrite parameter
+                    selected_chords=selected_chords,
+                    selected_inversions=selected_inversions,
+                    overwrite=options_dict["overwrite"],
                 )
     else:
         # Process just the single directory (no parallelization needed)
@@ -2905,7 +2907,9 @@ def interactive_mode():
             keep_artifacts=options_dict["keep_artifacts"],
             chord_qualities=chord_qualities,
             generate_inversions=generate_inversions,
-            overwrite=options_dict["overwrite"],  # Add overwrite parameter
+            selected_chords=selected_chords,
+            selected_inversions=selected_inversions,
+            overwrite=options_dict["overwrite"],
         )
 
     print_success("Processing complete!")
